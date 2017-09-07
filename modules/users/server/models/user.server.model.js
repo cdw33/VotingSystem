6cb7fd11c90c6ca7b0d7634baa5263626cf2c472
cdw33 <cdw33@zips.uakron.edu>
2017-09-07 17:19:11 -0400

    fixed passwords under 6 chars not being encrypted

--- modules/users/server/models/user.server.model.js
+++ modules/users/server/models/user.server.model.js
@@ -106,7 +106,7 @@ var UserSchema = new Schema({
       default: false
   },
   votedDate: {
-    type: Date    
+    type: Date
   }
 });
 
@@ -114,7 +114,7 @@ var UserSchema = new Schema({
  * Hook a pre save method to hash the password
  */
 UserSchema.pre('save', function (next) {
-  if (this.password && this.isModified('password') && this.password.length > 6) {
+  if (this.password && this.isModified('password')) {
     this.salt = crypto.randomBytes(16).toString('base64');
     this.password = this.hashPassword(this.password);
   }
